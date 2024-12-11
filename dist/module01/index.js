c7770be596ade952740f4069ee641a7405a6bd0d
unknown <atikurahad@gmail.com>
2024-12-11 12:37:52 +0600

    module01

---
+++ dist/module01/index.js
@@ -0,0 +1,7 @@
+"use strict";
+{
+    let name = "Hello";
+    console.log(name);
+    const age = 25;
+    console.log(age);
+}
---
+++ src/module01/index.ts
@@ -0,0 +1,7 @@
+{
+  let name: string = "Hello";
+  console.log(name);
+
+  const age: number = 25;
+  console.log(age);
+}
